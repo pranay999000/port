9e61c6b837dd5f3cc7c3b1fd20072df24988ce24
pranay999000 <35093184+pranay999000@users.noreply.github.com>
2021-02-07 13:25:14 +0530

    Retrying

--- src/components/IdForm.js
+++ src/components/IdForm.js
@@ -8,12 +8,20 @@ export default function IdForm({setid}) {
     const storeId = () => {
         if(id){
 
-            if(id < 0 || id > 20){
+            if(id < 1901201001 || id > 1901201116){
                 setMessage('Invalid registration Id')
-            }else if(id > 0 && id <= 10){
+            }else if(id > 1901201000 && id <= 1901201015){
+                localStorage.setItem('branch', 'civil')
+            }else if(id > 1901201015 && id <= 1901201068){
                 localStorage.setItem('branch', 'computer science')
-            }else if(id > 10 && id <=20){
+            }else if(id > 1901201068 && id <= 1901201078){
+                localStorage.setItem('branch', 'electrical electronics')
+            }else if(id > 1901201078 && id <= 1901201094){
+                localStorage.setItem('branch', 'electrical')
+            }else if(id > 1901201094 && id <= 1901201096){
                 localStorage.setItem('branch', 'electronics')
+            }else if(id > 1901201096 && id <= 1901201116){
+                localStorage.setItem('branch', 'mechanical')
             }
             
             localStorage.setItem('id', id)
